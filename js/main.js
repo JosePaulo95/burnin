0c3028714967aab6cb6aa7a76b46a9e1b7146c97
josepaulo <josepaulo@ceuma.br>
2022-01-22 16:26:12 -0300

    add as 3 seções do money bar

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ var App = new Vue({
   el: "#app",
   data() {
     return {
-      money_goal: 200,
-      money_amount: 50,
-      money_discount: 100
+      money_goal: 100,
+      money_amount: 25,
+      money_discount: 50
     }
   },
   beforeMount(){
@@ -15,24 +15,35 @@ var App = new Vue({
   },
   computed: {
     getLiquidPercentage(){
-      //let liquid = (this.money_amount-this.money_discount)/this.money_goal*100
-      let liquid = this.money_amount/this.money_goal*100
-      liquid = Math.floor(liquid)
+      let liquid = (this.money_amount-this.money_discount)/this.money_goal*100
       liquid = Math.min(100, liquid)
       liquid = Math.max(0, liquid)
+      liquid = Math.floor(liquid)
       return liquid
     },
-    getDiscountPercentage(){
+    getDiscountPartPercentage(){
       let discount = this.money_discount/this.money_goal*100
+      let amount = this.money_amount/this.money_goal*100
+
+      discount = Math.min(amount, discount)
       discount = Math.floor(discount)
-      discount = Math.min(100, discount)
-      discount = Math.max(0, discount)
       return discount
+    },
+    getExceededDiscountPercentage(){
+      let discount = this.money_discount/this.money_goal*100
+      let amount = this.money_amount/this.money_goal*100
+      let exceeded = discount-amount
+      exceeded = Math.max(0, exceeded)
+      exceeded = Math.floor(exceeded)
+      return exceeded
     }
   },
   methods: {
     start(){
 
     },
+    redeem_money(){
+      this.money_amount++
+    }
   }
 });
